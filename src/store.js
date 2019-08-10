f2832ee4f9138f9a8e501c8d3806564e4ae2a470
Andres2ya <andres.y.b@hotmail.com>
2019-08-10 18:34:06 -0500

    arreglo del store

--- src/store.js
+++ src/store.js
@@ -10,13 +10,20 @@ import REjemplo from './ducks/DuckEjemplo'
 //1. Configurando firebase en la aplicacion
 const firebaseConfig = {
     //TODO: Crear variables de entorno para estos datos
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyBZOHrSVxhH8B7D05Nfq9N2lvZmmx_khng",
+    authDomain: "xcala-01-2e539.firebaseapp.com",
+    databaseURL: "https://xcala-01-2e539.firebaseio.com",
+    projectId: "xcala-01-2e539",
+    storageBucket: "xcala-01-2e539.appspot.com",
+    messagingSenderId: "454505833977",
+    appId: "1:454505833977:web:c5847b0cd3c39f5f"
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // databaseURL: process.env.REACT_APP_DATABASE_URL,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);

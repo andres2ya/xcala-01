6758077b9dbf30c9c6ffd30b12e71f5463115daf
Andres2ya <andres.y.b@hotmail.com>
2019-08-10 14:29:24 -0500

    Se elimino app.css e index.css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from './store';
-import "./index.css";
 import App from "./App";
 
 

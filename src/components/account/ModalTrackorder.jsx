6cbfae1c76f6273c29be573441ee91550266f299
Andres2ya <andres.y.b@hotmail.com>
2019-09-06 23:57:48 -0500

    tracker order DONE.

--- src/components/account/ModalTrackorder.jsx
+++ src/components/account/ModalTrackorder.jsx
@@ -22,13 +22,43 @@ class ModalTrackorder extends Component {
         const {showTrackOrderModal}=this.props
         if(showTrackOrderModal){
             const {idTrackOrderData}=this.props
+            var onlyActiveStates=undefined;
+            var numberOfActiveStates=undefined;
+            if(idTrackOrderData){
+                onlyActiveStates=idTrackOrderData[0].estados.filter(estado=>estado.activo===true)
+                switch (onlyActiveStates.length) {
+                    case 1:
+                        numberOfActiveStates='One'
+                        break;
+                    case 2:
+                        numberOfActiveStates='Two'
+                        break;
+                    case 3:
+                        numberOfActiveStates='Three'
+                        break;
+                    case 4:
+                        numberOfActiveStates='Four'
+                        break;
+                    case 5:
+                        numberOfActiveStates='Five'
+                        break;
+                    case 6:
+                        numberOfActiveStates='Six'
+                        break;
+                    default:
+                        break;
+                }
+            }
             return (
               <div className="container modalTrackOrderInsideCard">
                 <div className="row modalTrackOrderContent">
                   <div className="col-12">
                     <div className="row trackOrderIdAndBackArrow">
                       <div className="col-8 numberOrderTitleModal d-flex justify-content-star align-items-center">
-                          Pedido #{idTrackOrderData?idTrackOrderData[0].numeroPedido:null}
+                        Pedido #
+                        {idTrackOrderData
+                          ? idTrackOrderData[0].numeroPedido
+                          : null}
                       </div>
                       <div className="col-4 d-flex justify-content-end">
                         <i
@@ -38,15 +68,26 @@ class ModalTrackorder extends Component {
                       </div>
                     </div>
                     <div className="row">
-                        <div className="col-12 paragraphOrderDetails d-flex align-items-center">
-                            Estado del pedido:
-                        </div>
+                      <div className="col-12 paragraphOrderDetailsModal d-flex align-items-center">
+                        Estado del pedido:
+                      </div>
                     </div>
-                    <div className="row orderStates">
-                        <div className="col-12 col-orderStates">
-                            Desplegar estados
-                        </div>
-                    </div>
+
+                    <div className={`axisStates axisStates-${numberOfActiveStates}`} />
+
+                    {onlyActiveStates
+                      ? onlyActiveStates.map(estado => (
+                          <div className="row orderStates">
+                            <div className="circleStatesOverAxis d-flex align-items-center">
+                              <div className="circleOverAxis" />
+                            </div>
+                            <div className="col-10 statesTextAxis d-flex justify-content-star align-items-center">
+                              {estado.fechaActivacion} | {estado.horaActivacion} - {estado.descripcion}
+                            </div>
+                          </div>
+                        ))
+                      : null}
+                      
                   </div>
                 </div>
               </div>

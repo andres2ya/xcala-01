254449442cff2dcc5947933f77319082c4c9ef15
Andres2ya <andres.y.b@hotmail.com>
2019-09-10 18:55:42 -0500

    main menu modified

--- src/components/account/MyAccount.jsx
+++ src/components/account/MyAccount.jsx
@@ -23,61 +23,122 @@ class MyAccount extends Component {
         if(!this.props.isAuth)
         return <Redirect to='/'/>
 
-        return (
+        return (          
             <div className="pcControlerScreen">
                 <div className="row">
                     <p className="accountTitle">Opciones de tu cuenta</p>   
                 </div>
-                <div className="row firstRow ">
-                    <div className="col-6 firstBoxAccountOption">
-                        <LinkWithDelay to='/order-details' delay={30}>    
-                            <div className="firstBoxAccountOptionOver" >
-                                
-                                <i className="icon-order-details-7px iconOpcion centerVerticalAndHorizontal d-flex justify-content-center"/>
-                                
-                                <span className="titleBoxAccountOption centerVerticalAndHorizontal d-flex justify-content-center">Detalles de tus pedidos</span>
-                                
+                <LinkWithDelay to="/order-details" delay={30}>
+                <div className="menuOptionsContainer">
+                    <div className="col-12 container">
+                        <div className="overMenuOpcionIcon firstOverMenuOptionIcon d-flex justify-content-center align-items-center">
+                            <i className="iconMenuOption firstIconMenuOption icon-order-details-7px centerVertical"></i>
+                        </div>
+                        <div className="row menuOpcionCard">
+                            <div className="col-12">
+                                <div className="row">
+                                        <div className="col-10 menuOptionTitleCard firstMenuOptionTitleCard d-flex  align-items-end">
+                                            <span>Detalles de tus pedidos</span>
+                                        </div>
+                                        <div className="col-2 iconArrowMenuOption firstIconArrowMenuOption arrowMenuOptionCard d-flex justify-content-center align-items-center">
+                                            <i className="icon-right-open-big centerVertical"></i>
+                                        </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-11 menuOptionDescriptionCard d-flex  align-items-start">
+                                        <span>Conoce el estado de tus pedidos y realiza reclamaciones.</span>
+                                    </div>
+                                </div>
                             </div>
-                        </LinkWithDelay>
-                    </div>
-                    <div className="col-6 secondBoxAccountOption">
-                        <LinkWithDelay to='/account-details' delay={30}>
-                            <div className="secondBoxAccountOptionOver">
-                                
-                                <i className="icon-accoun-tdetails-7px accountDetailsIcon iconOpcion centerVerticalAndHorizontal d-flex justify-content-center"/>
-                                
-                                <span className="titleBoxAccountOption centerVerticalAndHorizontal d-flex justify-content-center">Detalles de tu cuenta</span>
-                                
-                            </div>
-                        </LinkWithDelay>
-                    </div>
-                </div>
-                <div className="row secondRow">
-                    <div className="col-6 thirdBoxAccountOption ">
-                        <LinkWithDelay to='/invoice-address' delay={30}>
-                            <div className="thirdBoxAccountOptionOver">
-                                
-                                <i className="icon-invoice-address-7px invoiceAddresIcon iconOpcion centerVerticalAndHorizontal d-flex justify-content-center"/>
-                                
-                                <span className="titleBoxAccountOption centerVerticalAndHorizontal d-flex justify-content-center">Direccion de facturaccion</span>
-                                
-                            </div>
-                        </LinkWithDelay>
-                    </div>
-                    <div className="col-6 fourthBoxAccountOption">
-                        <div className="fourthBoxAccountOptionOverDos"/>
-                        <LinkWithDelay to='/shipping-address' delay={30}>
-                            <div className="fourthBoxAccountOptionOver">
-                                
-                                <i className="icon-shipping-address-7px iconOpcion centerVerticalAndHorizontal d-flex justify-content-center"/>
-                                
-                                <span className="titleBoxAccountOption centerVerticalAndHorizontal d-flex justify-content-center">Direcciones de envio</span>
-                                
-                            </div>
-                        </LinkWithDelay>
+                        </div>
                     </div>
                 </div>
+                </LinkWithDelay>
+                {/* SecondMenuOption */}
+
+                <div className="menuOptionsContainer">
+                    <div className="col-12 container">
+                        <div className="overMenuOpcionIcon secondOverMenuOptionIcon d-flex justify-content-center align-items-center">
+                            <i className="iconMenuOption secondIconMenuOption icon-accoun-tdetails-7px centerVertical"></i>
+                        </div>
+                        <div className="row menuOpcionCard">
+                            <div className="col-12">
+                                <div className="row">
+                                        <div className="col-10 menuOptionTitleCard secondMenuOptionTitleCard d-flex  align-items-end">
+                                            <span>Detalles de tu cuenta</span>
+                                        </div>
+                                        <div className="col-2 iconArrowMenuOption secondIconArrowMenuOption arrowMenuOptionCard d-flex justify-content-center align-items-center">
+                                            <i className="icon-right-open-big centerVertical"></i>
+                                        </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-11 menuOptionDescriptionCard d-flex  align-items-start">
+                                        <span>Administra los datos personales y de contacto de tu cuenta.</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* ThirdMenuOption */}
+
+                <div className="menuOptionsContainer">
+                    <div className="col-12 container">
+                        <div className="overMenuOpcionIcon thirdOverMenuOptionIcon d-flex justify-content-center align-items-center">
+                            <i className="iconMenuOption thirdIconMenuOption icon-invoice-address-7px centerVertical"></i>
+                        </div>
+                        <div className="row menuOpcionCard">
+                            <div className="col-12">
+                                <div className="row">
+                                        <div className="col-10 menuOptionTitleCard thirdMenuOptionTitleCard d-flex  align-items-end">
+                                            <span>Facturacion y pagos</span>
+                                        </div>
+                                        <div className="col-2 iconArrowMenuOption thirdIconArrowMenuOption arrowMenuOptionCard d-flex justify-content-center align-items-center">
+                                            <i className="icon-right-open-big centerVertical"></i>
+                                        </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-11 menuOptionDescriptionCard d-flex  align-items-start">
+                                        <span>Administra tu direccion de facturacion y cuenta bancaria.</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* fourthMenuOption */}
+
+                <div className="menuOptionsContainer fourthMenuOptionsContainer">
+                    <div className="col-12 container">
+                        <div className="overMenuOpcionIcon fourthOverMenuOptionIcon d-flex justify-content-center align-items-center">
+                            <i className="iconMenuOption fourthIconMenuOption icon-shipping-address-7px centerVertical"></i>
+                        </div>
+                        <div className="row menuOpcionCard">
+                            <div className="col-12">
+                                <div className="row">
+                                        <div className="col-10 menuOptionTitleCard fourthMenuOptionTitleCard d-flex  align-items-end">
+                                            <span>Direcciones de envio</span>
+                                        </div>
+                                        <div className="col-2 iconArrowMenuOption fourthIconArrowMenuOption arrowMenuOptionCard d-flex justify-content-center align-items-center">
+                                            <i className="icon-right-open-big centerVertical"></i>
+                                        </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-11 menuOptionDescriptionCard d-flex  align-items-start">
+                                        <span>Administra las direcciones de envio y detalles de tus clientes.</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 
+
+
+
                 <div className="row d-flex justify-content-center buttonLogout">
                     <button onClick={()=>this.props.logOut()}>Salir de la cuenta</button>
                 </div>

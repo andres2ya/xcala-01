55f036f878cf634965ba27003f6e3327e7025934
Andres2ya <andres.y.b@hotmail.com>
2020-01-16 20:52:11 -0500

    working on supplier side befor to deploy try

--- src/PruebasPayU.jsx
+++ src/PruebasPayU.jsx
@@ -8,18 +8,18 @@ export default class PruebasPayU extends Component {
                     <input name="merchantId"    type="hidden"  value="508029"   />
                     <input name="accountId"     type="hidden"  value="512321" />
                     <input name="description"   type="hidden"  value="Test PAYU"  />
-                    <input name="referenceCode" type="hidden"  value="PRUEBAXCALAuno" />
+                    <input name="referenceCode" type="hidden"  value="PRUEBAXCALA-13" />
                     <input name="amount"        type="hidden"  value="3"   />
                     <input name="tax"           type="hidden"  value="0"  />
                     <input name="taxReturnBase" type="hidden"  value="0" />
                     <input name="currency"      type="hidden"  value="USD" />
-                    <input name="signature"     type="hidden"  value="3e7fd1097ba0d7e9740375c9b4d39691"  />
+                    <input name="signature"     type="hidden"  value="5dcd621cc1d5ea73633854d7383caa4d"/>
                     <input name="test"          type="hidden"  value="1" />
                     <input name="buyerEmail"    type="hidden"  value="test@test.com" />
-                    <input name="responseUrl"    type="hidden"  value="http://www.test.com/response" />
-                    <input name="confirmationUrl"    type="hidden"  value="http://www.test.com/confirmation" />
+                    <input name="responseUrl"    type="hidden"  value="https://prueba-payu-deploy.firebaseapp.com/respuestapayu" />
+                    <input name="confirmationUrl"    type="hidden"  value="https://prueba-payu-deploy.firebaseapp.com/confirpayu" />
                     <label htmlFor="">Pruebas payu 1</label>
-                    <input name="Submit"        type="submit"  value="Enviar" />
+                    <input name="Submit"        type="submit"  value="Enviar6" />
                 </form>
             </div>
         )
